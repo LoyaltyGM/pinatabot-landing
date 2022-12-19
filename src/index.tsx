e93d38785642ee5ae643ab0f471245c59a2b5483
Vakurin <vakurin.maxim@gmail.com>
2022-12-19 23:39:58 +0200

    first row of landing and basic style

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import CustomCursor from "custom-cursor-react";
+// import CustomCursor from "custom-cursor-react";
+
+import "./styles/globals.css";
 
 function Landing() {
     return (
         <>
-            <div className="scrollbar-hide bg-black">
-                <CustomCursor
+            <div className="scrollbar-hide">
+                {/* <CustomCursor
                     targets={[".custom-cursor"]}
                     customClass="custom-cursor"
                     dimensions={45}
@@ -18,7 +20,23 @@ function Landing() {
                         opacity: 0.2,
                     }}
                     targetOpacity={0.5}
-                />
+                /> */}
+                <div className="p-3 flex gap-1 w-full">
+                    <div className="w-3/4 bg-white h-60 rounded-lg text-center items-center">
+                        <h1 className="text-black">LoyaltyGM</h1>
+                    </div>
+                    <div className="w-1/4 bg-white h-60 rounded-lg text-center items-center">
+                        <h1 className="text-black">LoyaltyGM</h1>
+                    </div>
+                    <div className="w-1/4 flex flex-col justify-between h-60 rounded-lg text-right items-right">
+                        <div className="">
+                            <p className="text-white">Twitter</p>
+                            <p className="text-white">Discord</p>
+                            <p className="text-white">Medium</p>
+                        </div>
+                        <button className="bg-[#CB36A6] py-3 rounded-2xl text-2xl">Open app</button>
+                    </div>
+                </div>
                 <h1>Hello!</h1>
             </div>
         </>
