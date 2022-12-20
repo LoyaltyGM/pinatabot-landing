3ad3d67d1d4d23148f3c70ee12838ccac39e9c2b
Vakurin <vakurin.maxim@gmail.com>
2022-12-20 20:59:25 +0200

    added icons

---
+++ src/assets/index.tsx
@@ -0,0 +1,15 @@
+// gif card
+import Card_LGM from "assets/gif/LGM_4mb.gif";
+
+// lootboxes
+import LBOX from "assets/lootbox/LBOX.png";
+import QR from "assets/QR-Code.png";
+const ASSETS = {
+    // card gif
+    Card_LGM: Card_LGM,
+    // lootboxes
+    LBOX: LBOX,
+    QR: QR,
+};
+
+export default ASSETS;
