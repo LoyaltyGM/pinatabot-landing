9669a9d8421c93a44c121068442f9b263279fef2
Vakurin <vakurin.maxim@gmail.com>
2022-12-20 22:11:16 +0200

    added tabs and finished with desktop version

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,11 @@
-import React, { useEffect } from "react";
+import React, { useEffect, Fragment, useState } from "react";
 import { createRoot } from "react-dom/client";
 import CustomCursor from "custom-cursor-react";
 import ASSETS from "assets";
 import "./styles/globals.css";
 import CircleType from "circletype";
 import ArrowUp from "components/icons/ArrowUp";
-
-function classNames(...classes) {
-    return classes.filter(Boolean).join(" ");
-}
+import { Tab } from "@headlessui/react";
 
 function Landing() {
     useEffect(() => {
@@ -16,6 +13,48 @@ function Landing() {
     }, []);
 
     const animationText = "Proof-of-loyalty NFT💳 ";
+    const tabInActive = "text-[#CCCCCC] px-4";
+    const tabActive = "bg-[#E6E6E6] text-[#E22BB5] rounded-2xl py-1 px-4 focus:outline-none ";
+
+    const MyTabs = () => {
+        return (
+            <Tab.Group>
+                <div className="flex items-center">
+                    <Tab.List className={"px-4 pt-4"}>
+                        <div className="flex py-2 px-2 border rounded-3xl border-[#E6E6E6]">
+                            <Tab as={Fragment}>
+                                {({ selected }) => (
+                                    <button className={selected ? tabActive : tabInActive}>
+                                        01
+                                    </button>
+                                )}
+                            </Tab>
+                            <Tab as={Fragment}>
+                                {({ selected }) => (
+                                    <button className={selected ? tabActive : tabInActive}>
+                                        02
+                                    </button>
+                                )}
+                            </Tab>
+                            <Tab as={Fragment}>
+                                {({ selected }) => (
+                                    <button className={selected ? tabActive : tabInActive}>
+                                        03
+                                    </button>
+                                )}
+                            </Tab>
+                        </div>
+                    </Tab.List>
+                    <div className="text-black pt-4">Loyalty System for Projects</div>
+                </div>
+                <Tab.Panels className={"pt-2 pl-4 text-black"}>
+                    <Tab.Panel>Content 1</Tab.Panel>
+                    <Tab.Panel>Content 2</Tab.Panel>
+                    <Tab.Panel>Content 3</Tab.Panel>
+                </Tab.Panels>
+            </Tab.Group>
+        );
+    };
 
     return (
         <>
@@ -33,10 +72,10 @@ function Landing() {
                 targetOpacity={0.5}
                 className="mt-0 pt-0"
             /> */}
-            <div className="scrollbar-hide h-[100vh] w-[100vw]">
-                <div className="px-3 pt-3 pb-1 flex gap-1 h-1/3 w-full">
-                    <div className="w-1/2 flex bg-white rounded-lg justify-center items-center">
-                        <h1 className="text-black font-bungee text-7xl">
+            <div className="scrollbar-hide lg:h-[100vh] w-[100vw]">
+                <div className="lg:flex px-3 pt-3 pb-1 gap-1 h-1/3 w-full">
+                    <div className="w-full lg:w-1/2 flex lg:bg-white rounded-lg justify-center items-center">
+                        <h1 className="text-black font-bungee lg:text-7xl text-4xl">
                             <span className="text-[#9747FF]">L</span>
                             <span className="text-[#C447FF]">O</span>
                             <span className="text-[#E547FF]">Y</span>
@@ -48,7 +87,7 @@ function Landing() {
                             <span className="text-[#6874E3]">M</span>
                         </h1>
                     </div>
-                    <div className="w-1/4 relative rounded-xl text-center items-center">
+                    <div className="lg:w-1/4 relative rounded-xl text-center items-center">
                         <img src={ASSETS.LBOX} className="w-full h-full object-cover" />
                         <div className="absolute bottom-0 left-0 pb-2 pl-4">Mystery Box</div>
                     </div>
@@ -85,7 +124,7 @@ function Landing() {
                 <div className="px-3 pb-1 flex w-full h-2/3 gap-1">
                     <div className="w-1/2 h-full gap-1">
                         <div className="h-2/3 pb-1 flex gap-1">
-                            <div className="h-full w-1/2 bg-[#CB36A6] rounded-2xl p-6 flex flex-col justify-center items-center">
+                            <div className="hidden h-full w-1/2 bg-[#CB36A6] rounded-2xl p-6 lg:flex flex-col justify-center items-center">
                                 <img src={ASSETS.QR} className="w-3/4 h-1/2 object-contain" />
                                 <p className="text-xl font-light">SCAN ME</p>
                             </div>
@@ -104,7 +143,7 @@ function Landing() {
                             </div>
                         </div>
                         <div className="h-1/3 w-full bg-white rounded-2xl">
-                            <div>ggg</div>
+                            <MyTabs />
                         </div>
                     </div>
                     <div className="w-1/2 relative bg-white h-full rounded-lg">
