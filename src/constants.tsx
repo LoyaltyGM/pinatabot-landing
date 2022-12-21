5eb932282550d5fb73fdcbcd127d184984feca4e
Vakurin <vakurin.maxim@gmail.com>
2022-12-21 16:33:32 +0200

    constants with main links

---
+++ src/constants.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+export const TWITTER_URL = "https://twitter.com/Loyalty_GM";
+export const MEDIUM_URL = "https://medium.com/@loyaltygm";
+// TODO: Add WL FORM URL
+export const APPLY_WL_FORM = "";
+export const MYSTERY_BOX_URL = "https://app.loyaltygm.com/lootbox";
+export const REF_LOYALTY_URL = "https://app.loyaltygm.com/waitlist";
+
+export const tabsContentTitle = [
+    "Proof-Of-Loyalty NFTs",
+    "NFT Loyalty card that collects user activity",
+    "Rewards for both users and companies",
+];
+
+export const tabsContent = [
+    "Companies can create their own loyalty NFTs with limited supply and link them to on-chain activities or social media engagement on our platform",
+    "We have developed a system that verifies and assigns XP to NFTs based on the user's activity",
+    "With loyalty NFTs, companies can easily reward users who contribute value to the company. This helps companies recognize and incentivize their most dedicated and " +
+        "valuable users, increasing user engagement and retention",
+];
